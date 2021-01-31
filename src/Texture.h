c8ae495e98ade5f81f9a0c97aa7b4e74fc45018a
Adam Osiowy <adamosiowy@wp.pl>
2021-01-31 13:42:39 +0100

    Load particle textures

--- src/Texture.h
+++ src/Texture.h
@@ -2,6 +2,7 @@
 
 #include "glew.h"
 #include "freeglut.h"
+#include <vector>
 
 namespace Core
 {
@@ -12,4 +13,5 @@ namespace Core
 	// programID - identyfikator aktualnego programu karty graficznej
 	// textureUnit - indeks jednostki teksturujacej - liczba od 0 do 7. Jezeli uzywa sie wielu tekstur w jednym shaderze, to kazda z nich nalezy powiazac z inna jednostka.
 	void SetActiveTexture(GLuint textureID, const char * shaderVariableName, GLuint programID, int textureUnit);
+	std::vector<GLuint> LoadParticleTextures();
 }
