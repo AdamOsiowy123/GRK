719838594795ecc2771c6d3d237b3c940b2732a6
Adam Osiowy <adamosiowy@wp.pl>
2021-01-31 15:45:43 +0100

    particle effect update

--- src/ParticleEffect.h
+++ src/ParticleEffect.h
@@ -20,7 +20,7 @@ struct Particle {
     float Age;
     float Life;
 
-    Particle(glm::vec3 pos) : Position(glm::vec3(pos.x + (rand() %5 ) / 10.0f, pos.y + (rand() % 5) / 10.0f, pos.z + (rand() % 5) / 10.0f)), Velocity(glm::vec3(((rand() % 5) -10) / 10.0f, ((rand() % 5) -10)/ 10.0f, ((rand() % 5) -10) / 10.0f)), Age(0.0f), Life(1.0f) { }
+    Particle(glm::vec3 pos) : Position(glm::vec3(pos.x + (rand() %5 ) / 10.0f, pos.y + (rand() % 5) / 10.0f, pos.z + (rand() % 5) / 10.0f)), Velocity(glm::vec3(0.0f, ((rand() % 5) -10) * 100.0f, ((rand() % 5) -10) * 100.0f)), Age(0.0f), Life(1.0f) { }
     bool operator<(Particle& that) {
         // old particles drawn first
         return this->Age > that.Age;
